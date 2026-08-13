a23d3d6f15831440c5d74ebb1831f59feb3c5a0d
Nayerlis <nrmirandav@academia.usbbog.edu.co>
2026-08-12 20:51:44 -0500

    codigo realizado en clase

---
+++ clase1/variable.js
@@ -0,0 +1,76 @@
+/*let number=10;
+const otherNumber=15;
+
+number=8;
+otherNumber=8;
+
+const a=true;
+const b=false;
+
+//AND los dos sean verdaderos
+if(a== true && b==true) { 
+
+}
+
+//OR al menos 1 es verdadero
+if(a== true|| b==true) { 
+
+}
+
+//NOT negacion
+if(a !=false){
+}
+
+if(1=="1")
+    { 
+        true==1
+        undefined==0
+//Solo compara valores
+}
+
+if(1===1)
+    { 
+//Comparacion estricta y compara valor y el tipo
+}
+
+
+//ATRAYS
+//               0         1         2       3
+ let students = ["Angela", "Andres", "Jade", "Hanna"];
+
+console.log(students);
+
+students[1] = "Andres Esquivia";
+students[3] = "Hanna Castano";
+
+students.push("Vivi");
+
+const student = students.pop();
+
+console.log(students);
+console.log(student);
+
+for (let i = 0; i < students.length; i++) {
+    console.log(students[i]);
+}
+
+while (students.length > 0) {
+    console.log(students.pop());
+}
+
+*/
+//OBJETOS
+Datos = {
+    name: "Carlos",
+    age: 42,
+
+};
+
+pepito = [
+    {name: "DB ", teacher: "Ing. pepe"},
+    {name: "CF ", teacher: "Ing. pepe"},
+    {name: "GK ", teacher: "Ing. pepe"}
+]
+console.log(Datos.age);
+console.log(pepito[1].teacher);
+/*Datos.getAge();*/
