c8093716649e75caea487fa8f3818d7dffdcae38
Nayerlis <nrmirandav@academia.usbbog.edu.co>
2026-08-19 21:38:13 -0500

    clase 3

---
+++ clase3/Eventos.js
@@ -0,0 +1,39 @@
+//evento loop 
+//ciclo de eventos de javascrit
+
+//esta hecho para que javasrcirp pueda esperar 
+
+//es como hcer una sopa : calentar, cortar la papa  cortar la vedura y echar la olla 
+
+//setTimeout(() => { console.log("Hola 3"); }, 0);
+//Promise.resolve().then( () => console.log("Hola 2"));
+//console.log("Hola 1")
+//PS C:\Users\406.laboratorios\Music> node eventloop.js
+//Hola 1
+//Hola 2
+//Hola 3
+//PS C:\Users\406.laboratorios\Music> 
+
+
+//------------------------
+
+//API restfull
+//aplicación programación interface (es como un contrato)
+//counicacon=> es un intermediario 
+
+//usamos el desarrrolo de backend 
+
+//para que una api sea rest debe ser cliente servidor 
+//debe ser sin estado = es como la primara vez que lo conoce 
+//el cache es mandar infomacion extra 
+//también tine sistemas por capas 
+//inrefaz uniforme tiene que funcionar siempre de la misma manera 
+//-------------------------------------------------------------------
+//solo los principios que vamos a aplicar
+//1. cliente servidor 
+//2. interzas uniforme
+//3. cache
+//4. layerd
+//5. code-an-demand
+//buscar verbos http : get, post, put, delete, patch
+//solo vamos a usar get y post
